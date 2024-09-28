877d37782fb429988ab14aadb0158329c725ebf2
binbinmods <binbinmods@gmail.com>
2024-09-28 00:59:08 -0400

    Initial Commit

---
+++ UlfvitrTraits/Plugin.cs
@@ -0,0 +1,37 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using BepInEx.Configuration;
+using HarmonyLib;
+using static Obeliskial_Essentials.Essentials;
+
+namespace TheWiseWolf
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    [BepInDependency("com.stiffmeds.obeliskialessentials")]
+    [BepInDependency("com.stiffmeds.obeliskialcontent")]
+    [BepInProcess("AcrossTheObelisk.exe")]
+    public class Plugin : BaseUnityPlugin
+    {
+        internal const int ModDate = 20240916;
+        private readonly Harmony harmony = new(PluginInfo.PLUGIN_GUID);
+        internal static ManualLogSource Log;
+        private void Awake()
+        {
+            Log = Logger;
+            Log.LogInfo($"{PluginInfo.PLUGIN_GUID} {PluginInfo.PLUGIN_VERSION} has loaded!");
+            // register with Obeliskial Essentials
+            RegisterMod(
+                _name: PluginInfo.PLUGIN_NAME,
+                _author: "binbin",
+                _description: "Ulfvitr, The Wise Wolf.",
+                _version: PluginInfo.PLUGIN_VERSION,
+                _date: ModDate,
+                _link: @"https://github.com/binbinmods/ulfvitrwisewolf",
+                _contentFolder: "Ulfvitr",
+                _type: ["content", "hero", "trait"]
+            );
+            // apply patches
+            harmony.PatchAll();
+        }
+    }
+}
