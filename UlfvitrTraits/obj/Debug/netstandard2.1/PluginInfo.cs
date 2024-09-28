be0b130cf181d6e06fb19743a86d807f50c3f714
binbinmods <binbinmods@gmail.com>
2024-09-28 16:48:16 -0400

    Fixed packing and auto-zip

--- UlfvitrTraits/obj/Debug/netstandard2.1/PluginInfo.cs
+++ UlfvitrTraits/obj/Debug/netstandard2.1/PluginInfo.cs
@@ -4,6 +4,6 @@ namespace TheWiseWolf
     {
         public const string PLUGIN_GUID = "com.binbin.TheWiseWolf";
         public const string PLUGIN_NAME = "The Wise Wolf";
-        public const string PLUGIN_VERSION = "1.0.0";
+        public const string PLUGIN_VERSION = "1.0.2";
     }
 }
--- shipUlfvitr.py
+++ shipUlfvitr.py
@@ -7,8 +7,9 @@ import json
 target_name = "Ulfvitr, The Wise Wolf"
 csproj_name = "TheWiseWolf"
 
-enabled = False
-load_to_local_ato = True
+enabled = True
+load_to_local_ato = False
+doZip = True
 
 
 # path to source directory
@@ -19,7 +20,7 @@ char_name = script_dir.split("/")[-1]
 config_dir = script_dir + f"/{char_name}Configs"
 
 # path to destination directory
-config_dest_dir = os.path.abspath(os.path.join(script_dir, '..', f'Ready to Ship/{target_name}/BepInEx/config/Obeliskial_importing'))
+config_dest_dir = os.path.abspath(os.path.join(script_dir, '..', f'Ready to Ship/{target_name}/BepInEx/config/Obeliskial_importing/{char_name}'))
 
 
 #Functions
@@ -33,36 +34,46 @@ def copy_file(from_path,to_path,file):
         os.remove(to_path+"/"+file)
     shutil.copyfile(from_path, to_path+"/"+file)
 
-# copy configs
-if enabled:
-    copy_folders(config_dir, config_dest_dir)
-#print(os.listdir(config_dest_dir))
+def main():
+    # copy configs
+    if enabled:
+        copy_folders(config_dir, config_dest_dir)
+    #print(os.listdir(config_dest_dir))
 
-# copy dll
-plugin_name = f"com.binbin.{csproj_name}.dll"
-trait_dir = script_dir + f"/{char_name}Traits/bin/Debug/netstandard2.1/{plugin_name}"
-trait_dest_dir = os.path.abspath(os.path.join(script_dir, '..', f'Ready to Ship/{target_name}/BepInEx/plugins'))
+    # copy dll
+    plugin_name = f"com.binbin.{csproj_name}.dll"
+    trait_dir = script_dir + f"/{char_name}Traits/bin/Debug/netstandard2.1/{plugin_name}"
+    trait_dest_dir = os.path.abspath(os.path.join(script_dir, '..', f'Ready to Ship/{target_name}/BepInEx/plugins'))
 
-if enabled:
-    copy_file(trait_dir,trait_dest_dir,plugin_name)
+    if enabled:
+        copy_file(trait_dir,trait_dest_dir,plugin_name)
 
-# update version number
-if enabled:
-    with open(os.path.abspath(os.path.join(script_dir, '..', f'Ready to Ship/{target_name}/manifest.json')),"r+") as json_data:
-        manifest = json.load(json_data)
-        version = manifest["version_number"].split(".")
-        version[-1]=str(int(version[-1])+1)
-        manifest["version_number"] = ".".join(version)
-        json_data.seek(0)       
-        json.dump(manifest, json_data, indent=4)
-        json_data.truncate()     
+    # update version number
+    if enabled:
+        with open(os.path.abspath(os.path.join(script_dir, '..', f'Ready to Ship/{target_name}/manifest.json')),"r+") as json_data:
+            manifest = json.load(json_data)
+            version = manifest["version_number"].split(".")
+            version[-1]=str(int(version[-1])+1)
+            manifest["version_number"] = ".".join(version)
+            json_data.seek(0)       
+            json.dump(manifest, json_data, indent=4)
+            json_data.truncate()     
 
-if load_to_local_ato:
-    ato_importing = f"/Users/kevinmccoy/Library/Application Support/Steam/steamapps/common/Across the Obelisk/BepInEx/config/Obeliskial_importing/{char_name}"
-    ato_plugins = "/Users/kevinmccoy/Library/Application Support/Steam/steamapps/common/Across the Obelisk/BepInEx/plugins"
-    copy_folders(config_dir,ato_importing)
-    copy_file(trait_dir,ato_plugins,plugin_name)
-    print("Added to local!")
+    if load_to_local_ato:
+        ato_importing = f"/Users/kevinmccoy/Library/Application Support/Steam/steamapps/common/Across the Obelisk/BepInEx/config/Obeliskial_importing/{char_name}"
+        ato_plugins = "/Users/kevinmccoy/Library/Application Support/Steam/steamapps/common/Across the Obelisk/BepInEx/plugins"
+        copy_folders(config_dir,ato_importing)
+        copy_file(trait_dir,ato_plugins,plugin_name)
+        print("Added to local!")
 
-if enabled:
-    print("Hopefully Worked!")
+    if enabled:
+        print("Hopefully Worked!")
+
+    if doZip:
+        print("zipping")
+        output_filename = f"{target_name}"
+        zip_dir = os.path.abspath(os.path.join(script_dir, '..', f'Ready to Ship/{target_name}'))
+        shutil.make_archive(output_filename, 'zip',zip_dir)
+
+
+main()
