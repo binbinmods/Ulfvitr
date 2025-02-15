c59a5852ae90bce728eed1e1b0fe5961c57f76b9
binbinmods <binbinmods@gmail.com>
2025-02-14 23:26:13 -0500

    Removed unnecessary folders

--- UlfvitrTraits/obj/Debug/netstandard2.1/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace TheWiseWolf
-{
-    public static class PluginInfo
-    {
-        public const string PLUGIN_GUID = "com.binbin.TheWiseWolf";
-        public const string PLUGIN_NAME = "The Wise Wolf";
-        public const string PLUGIN_VERSION = "1.0.2";
-    }
-}
